4b95c0d7c411a4c3249aafe994c721c583f10d9f
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-09-21 12:29:04 +0530

    Wraps the application in a Redux Provider to make the store accessible to all components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux"; // ✅ import Provider
+import App from "./App.tsx";
+import { store } from "./redux/store"; // ✅ import your store
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Provider store={store}> {/* ✅ Wrap your App in Provider */}
+            <App />
+        </Provider>
+    </StrictMode>
+);
